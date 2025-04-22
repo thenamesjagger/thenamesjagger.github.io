1a06f3c344b5468cdcea88a3470f4ccc8af4a1e2
thenamesjagger <67752390+thenamesjagger@users.noreply.github.com>
2025-04-22 10:01:07 -0700

    Update tailwind-config.js

--- tailwind-config.js
+++ tailwind-config.js
@@ -1,25 +1,22 @@
-<script>
-  tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          primary: '#4F46E5',
-          secondary: '#10B981',
-        },
-        borderRadius: {
-          'none': '0px',
-          'sm': '4px',
-          DEFAULT: '8px',
-          'md': '12px',
-          'lg': '16px',
-          'xl': '20px',
-          '2xl': '24px',
-          '3xl': '32px',
-          'full': '9999px',
-          'button': '8px',
-        }
-      }
-    }
-  };
-</script>
-
+tailwind.config = {
+   theme: {
+     extend: {
+       colors: {
+         primary: '#4F46E5',
+         secondary: '#10B981',
+       },
+       borderRadius: {
+         'none': '0px',
+         'sm': '4px',
+         DEFAULT: '8px',
+         'md': '12px',
+         'lg': '16px',
+         'xl': '20px',
+         '2xl': '24px',
+         '3xl': '32px',
+         'full': '9999px',
+         'button': '8px',
+       }
+     }
+   }
+ };
